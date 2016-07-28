37d734592f7a780b89256fc65e5ef026f42214ec
liuxq <lxqiang87@126.com>
2016-07-28 18:52:26 +0800

    go

---
+++ Assets/Script/MoveController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+    private Animation ani;
+    private SmoothFollow sf;
+    private CharacterController cc;
+    private Vector2 pos;
+    private bool isPressDown;
+
+    void OnEnable()
+    {
+        Debug.Log("Registering finger gesture events from C# script");
+
+        // register input events
+        FingerGestures.OnFingerDown += OnFingerDown;
+        FingerGestures.OnFingerUp += OnFingerUp;
+    }
+
+    void OnDisable()
+    {
+        // unregister finger gesture events
+        FingerGestures.OnFingerDown -= OnFingerDown;
+        FingerGestures.OnFingerUp -= OnFingerUp;
+    }
+
+    void Start()
+    {
+        ani = GetComponent<Animation>();
+        sf = Camera.main.GetComponent<SmoothFollow>();
+        cc = GetComponent<CharacterController>();
+        sf.ResetView();
+    }
+    void OnFingerDown(int fingerIndex, Vector2 fingerPos)
+    {
+        isPressDown = true;
+        pos = fingerPos;
+    }
+    void OnFingerUp(int fingerIndex, Vector2 fingerPos, float timeHeldDown)
+    {
+        isPressDown = false;
+        ani.CrossFade("Idle");
+    }
+    void Update()
+    {
+        if(isPressDown)
+        {
+            //设置角色的朝向（朝向当前坐标+摇杆偏移量）
+            Quaternion r = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
+
+            Vector3 dir = r * new Vector3(Input.mousePosition.x - pos.x, 0, Input.mousePosition.y - pos.y);
+            transform.LookAt(new Vector3(transform.position.x + dir.x, transform.position.y, transform.position.z + dir.z));
+
+            //移动玩家的位置（按朝向位置移动）
+            //transform.Translate(Vector3.forward * Time.deltaTime * 2);
+            dir.Normalize();
+            cc.Move(dir * Time.deltaTime * 2);
+            //移动摄像机
+            sf.FollowUpdate();
+            //播放奔跑动画
+            //ani.wrapMode = WrapMode.Loop;
+            ani.CrossFade("Walk");
+        }
+        
+    }
+   
+
+}
---
+++ Assets/Shader/flash/flash.shader
@@ -0,0 +1,52 @@
+﻿Shader "Custom/flash" {
+	Properties {
+        _MainTex ("Base (RGB)", 2D) = "white" {}
+        _FlashColor ("Flash Color", Color) = (1,1,1,1)
+        _Width ("Flash Width", Range(0, 1)) = 0.2
+        _LoopTime ("Loop Time", Float) = 1
+        _Interval ("Time Interval", Float) = 3
+    }
+    SubShader
+    {
+    	Tags { "Queue"="Transparent" "RenderType"="Opaque" }
+    
+        CGPROGRAM
+        #pragma surface surf Lambert exclude_path:prepass noforwardadd
+		//#pragma target 3.0
+
+        sampler2D _MainTex;
+        half4 _FlashColor;
+        half _Width;
+        half _LoopTime;
+        half _Interval;
+            
+        struct Input 
+		{
+			half2 uv_MainTex;
+		};
+           
+        half inFlash(half2 uv)
+        {	            
+            half cot = 0.3;//光条角度
+           
+			half xBottomRightBound = (_Time.y - (int)_Time.y) * (1.0 + cot);
+            half xBottomLeftBound = xBottomRightBound - _Width;
+            
+            half xProj = uv.x + uv.y * cot;
+
+            return xProj > xBottomLeftBound && xProj < xBottomRightBound ? 1.0 - abs(2.0 * xProj - (xBottomLeftBound + xBottomRightBound)) / _Width : 0;
+        }
+        
+        void surf (Input IN, inout SurfaceOutput o)
+        {                
+            half4 texCol = tex2D(_MainTex, IN.uv_MainTex);
+			half brightness = inFlash(IN.uv_MainTex);
+            o.Albedo = texCol.rgb + _FlashColor.rgb * brightness;
+            o.Alpha = texCol.a;
+        }
+        
+        ENDCG
+    }
+    
+    FallBack "Unlit/Transparent"
+}
