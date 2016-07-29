ac6cda2809cf21607501da5448cd8caefe9d7f73
liuxq <lxqiang87@126.com>
2016-07-29 09:17:57 +0800

    12

---
+++ Assets/Script/Editor/Packager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+
+public class Packager{
+
+    static List<AssetBundleBuild> maps = new List<AssetBundleBuild>();
+
+    [MenuItem("TestGame/Build iPhone Resource", false, 100)]
+    public static void BuildiPhoneResource()
+    {
+        BuildTarget target;
+#if UNITY_5
+        target = BuildTarget.iOS;
+#else
+        target = BuildTarget.iPhone;
+#endif
+        BuildAssetResource(target);
+    }
+
+    [MenuItem("TestGame/Build Android Resource", false, 101)]
+    public static void BuildAndroidResource()
+    {
+        BuildAssetResource(BuildTarget.Android);
+    }
+
+    [MenuItem("TestGame/Build Windows Resource", false, 102)]
+    public static void BuildWindowsResource()
+    {
+        BuildAssetResource(BuildTarget.StandaloneWindows);
+    }
+
+    /// <summary>
+    /// 生成绑定素材
+    /// </summary>
+    public static void BuildAssetResource(BuildTarget target)
+    {
+        if (Directory.Exists(Util.DataPath))
+        {
+            Directory.Delete(Util.DataPath, true);
+        }
+        string streamPath = Application.streamingAssetsPath;
+        if (Directory.Exists(streamPath))
+        {
+            Directory.Delete(streamPath, true);
+        }
+        Directory.CreateDirectory(streamPath);
+        AssetDatabase.Refresh();
+
+        maps.Clear();
+        if (AppConst.LuaBundleMode)
+        {
+            HandleLuaBundle();
+        }
+        else
+        {
+            HandleLuaFile();
+        }
+        if (AppConst.ExampleMode)
+        {
+            HandleExampleBundle();
+        }
+        HandleResBundle();//资源打包
+        string resPath = "Assets/" + "StreamingAssets";
+        BuildAssetBundleOptions options = BuildAssetBundleOptions.DeterministicAssetBundle |
+                                          BuildAssetBundleOptions.UncompressedAssetBundle;
+        BuildPipeline.BuildAssetBundles(resPath, maps.ToArray(), options, target);
+        BuildFileIndex();
+
+        string streamDir = Application.dataPath + "/" + AppConst.LuaTempDir;
+        if (Directory.Exists(streamDir)) Directory.Delete(streamDir, true);
+        AssetDatabase.Refresh();
+    }
+}
